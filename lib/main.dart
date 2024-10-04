2c927e2d0cd2510bf211c1a504f02466d0962cd9
Eduarda Portel <eduarda.portel4@gmail.com>
2024-10-04 13:03:58 -0300

    WebLayout implementado

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,83 @@ class MeuLayoutResponsivo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Aplicação responvidade")),
+      appBar: AppBar(title: Text("Aplicação responsiva simples")),
       body: LayoutBuilder(builder: (context, constraints){ 
-        return constraints.maxWidth <= 576 ? Text("Pequena") : Text("Grande"); //ternário
+        return constraints.maxWidth <= 576 ? MobileLayout() : Text("Grande"); //ternário
       }),
     );
   }
 }
+
+class MobileLayout extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MobileLayoutState();
+  }
+}
+
+class MobileLayoutState extends State<MobileLayout> with SingleTickerProviderStateMixin {
+  TabController? tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    //TabController: gerencia a seleção de abas e as animações entre elas
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // TabBar: Widget que exibe uma barra de abas
+        TabBar(
+          labelColor: Colors.black,
+          controller: tabController,
+          tabs: [Tab(text: "Aba 1"), Tab(text: 'Aba 2')],
+        ),
+
+        Expanded(
+          // TabBarView: Widget que exibe o conteúdo de uma aba selecionada
+          child: TabBarView(
+            controller: tabController,
+            children: [
+              Center(child: Text("Conteúdo da primeira aba")),
+              Center(child: Text("Conteúdo da segunda aba"))
+            ],
+        ))
+      ],
+    );
+  }
+}
+
+class WebLayout extends StatefulWidget { 
+  @override
+  State<StatefulWidget> createState() {
+    return WebLayoutState();
+  }
+}
+
+// definição do Widget (e de seu estado) para telas maiores.
+class WebLayoutState extends State<WebLayout> {
+  @override 
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: Card(
+            child: Center(child: Text("Conteúdo da primeira aba"))  
+          )
+        ),
+        Expanded(
+          child: Card(
+            child: Center(
+              child: Text('Conteúdo da segunda aba'),
+            )
+          )
+        )
+      ],
+    );
+  }
+}
+
